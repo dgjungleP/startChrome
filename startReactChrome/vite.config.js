c010aae69549367617115788d45189322794ab7e
dgjungleP <dgJungle_c@163.com>
2022-01-25 10:12:23 +0800

    vite copy file

--- startReactChrome/vite.config.js
+++ startReactChrome/vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, optimizeDeps } from "vite";
+import react from "@vitejs/plugin-react";
+import copy from "rollup-plugin-copy";
+import { resolve } from "path";
+const path = require("path");
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [
+    react(),
+    copy({
+      targets: [{ src: "./public/manifest.json", dest: "./dist" }],
+    }),
+  ],
+  base: "./",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
