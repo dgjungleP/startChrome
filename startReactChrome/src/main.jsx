61b42da3b6333f2d0d38b482746daf97f88984af
dgjungle_c <dgjungle_c@163.com>
2023-03-11 17:49:13 +0800

    add notification

--- startReactChrome/src/main.jsx
+++ startReactChrome/src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom";
 import "antd/dist/antd.css";
 import "./main.css";
 import { Time } from "./compoment/dashboard";
+import { TabInfo } from "./compoment/chrome";
 
 ReactDOM.render(
   <React.StrictMode>
     <Time />
+    <TabInfo></TabInfo>
   </React.StrictMode>,
   document.getElementById("root")
 );
