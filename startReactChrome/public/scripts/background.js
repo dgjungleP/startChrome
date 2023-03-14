159d3d719c54447cf22f47ac65725ba606ffbc67
dgjungle_c <dgjungle_c@163.com>
2023-03-14 15:45:30 +0800

    support notification

--- startReactChrome/public/scripts/background.js
+++ startReactChrome/public/scripts/background.js
@@ -9,26 +9,16 @@ chrome.runtime.onInstalled.addListener(() => {
 function windowsNotify(content) {
   console.log(content);
   // 判断浏览器是否支持唤醒
-  if (window.Notification) {
-    let popNotice = () => {
-      if (!Notification.permission === "granted") return;
-      const notification = new Notification("JungleTool", {
-        body: content,
-      });
-      // 点击通知的回调函数
-      notification.onclick = function () {
-        window.open("https://baidu.com");
-        notification.close();
-      };
-    };
-    /* 授权过通知 */
-    if (Notification.permission === "granted") {
-      popNotice();
-    } else {
-      /* 未授权，先询问授权 */
-      Notification.requestPermission(function (permission) {
-        popNotice();
-      });
+  chrome.notifications.create(
+    (Math.random() * 1000).toFixed(0),
+    {
+      message: content,
+      type: "basic",
+      title: "HelloJungle",
+      iconUrl: "/images/icon-32.png",
+    },
+    (id) => {
+      console.log(id);
     }
-  }
+  );
 }
